c77fdf159676ab6bf845893f0fbb8be23ee592ee
Miguel Bogota <miguel.bogota@mercadolibre.com.co>
2025-01-06 13:24:41 -0500

    Adds styles utils, some config and updates index.html

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   base: process.env.BASE_PATH ?? '/',
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
   server: {
     port: 4000,
   },
